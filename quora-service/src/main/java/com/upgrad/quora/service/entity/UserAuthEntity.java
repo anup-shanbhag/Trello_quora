593bbd4f8505190a2ff976f3a9152630989ec241
narayaa <ashik.narayan@ig.com>
2020-04-10 22:24:52 +0530

    Common Controller feature working - all tests passed from CommonControllerTest

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthEntity.java
@@ -0,0 +1,115 @@
+package com.upgrad.quora.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_auth")
+@NamedQueries({
+        @NamedQuery(name = "userAuthTokenByAccessToken", query = "select ut from UserAuthEntity ut where ut.accessToken=:accessToken")
+})
+public class UserAuthEntity implements Serializable {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "ACCESS_TOKEN")
+    @Size(max = 500)
+    @NotNull
+    private String accessToken;
+
+    @Column(name = "EXPIRES_AT")
+    @NotNull
+    private LocalDateTime expiresAt;
+
+    @Column(name = "LOGIN_AT")
+    @NotNull
+    private LocalDateTime loginAt;
+
+    @Column(name = "LOGOUT_AT")
+    private LocalDateTime logoutAt;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public LocalDateTime getLoginAt() {
+        return loginAt;
+    }
+
+    public void setLoginAt(LocalDateTime loginAt) {
+        this.loginAt = loginAt;
+    }
+
+    public LocalDateTime getLogoutAt() {
+        return logoutAt;
+    }
+
+    public void setLogoutAt(LocalDateTime logoutAt) {
+        this.logoutAt = logoutAt;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
