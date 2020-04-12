07c41b38b63c1ce1259070aa3f1d34059d090249
narayaa <ashik.narayan@ig.com>
2020-04-12 15:38:21 +0530

    User Controller feature - signup feature working without unique key exception handler

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.UserBusinessService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    @Autowired
+    private UserBusinessService userBusinessService;
+
+    /**
+     * This is used to register a user in the application. It takes input for content of the user and register in the database
+     *
+     * @param user An input request with user content
+     * @return Response Entity with userUuId, message and Http Status Code
+     * @throws SignUpRestrictedException if the username or email already exists
+     */
+    @RequestMapping(method = RequestMethod.POST,
+            path = "/user/signup",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> registerUser(SignupUserRequest user) {
+        UserEntity newUserEntiry = new UserEntity();
+
+        newUserEntiry.setFirstName(user.getFirstName());
+        newUserEntiry.setLastName(user.getLastName());
+        newUserEntiry.setUserName(user.getUserName());
+        newUserEntiry.setEmail(user.getEmailAddress());
+        newUserEntiry.setPassword(user.getPassword());
+        newUserEntiry.setCountry(user.getCountry());
+        newUserEntiry.setAboutMe(user.getAboutMe());
+        newUserEntiry.setDob(user.getDob());
+        newUserEntiry.setContactNumber(user.getContactNumber());
+        newUserEntiry.setRole("nonadmin");
+        newUserEntiry.setUuid(UUID.randomUUID().toString());
+        newUserEntiry.setSalt("quora123");
+
+        UserEntity createdUser = userBusinessService.registerUser(newUserEntiry);
+        SignupUserResponse userResponse = new SignupUserResponse()
+                .id(createdUser.getUuid())
+                .status("USER SUCCESSFULLY REGISTERED");
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+}
