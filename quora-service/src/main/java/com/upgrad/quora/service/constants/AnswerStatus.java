dcf360734f4661c68dcb215b7db48f53f2ae427b
alok.saboji@gmail.com”git config --global user.email alok.saboji@gmail.comgit initgit config --global user.name alok.saboji@gmail.com” <alok.saboji@gmail.com>
2020-04-14 07:11:09 +0530

    Delete Answer, all test case pass

--- quora-service/src/main/java/com/upgrad/quora/service/constants/AnswerStatus.java
+++ quora-service/src/main/java/com/upgrad/quora/service/constants/AnswerStatus.java
@@ -2,7 +2,7 @@ package com.upgrad.quora.service.constants;
 
 public enum AnswerStatus {
 
-    ANSWER_CREATED("ANSWER CREATED"),ANSWER_EDITED("ANSWER EDITED");
+    ANSWER_CREATED("ANSWER CREATED"),ANSWER_EDITED("ANSWER EDITED"),ANSWER_DELETED("ANSWER DELETED");
     private String textStatus;
 
     AnswerStatus(String textStatus) {
